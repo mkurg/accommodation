8011a494aac1cdd4e8cbfebfee6a01bfbc8b71b3
Matvei Kurzukov <mkurg@ya.ru>
2017-05-30 03:32:41 +0300

    Improve

--- scripts/get_threads.py
+++ scripts/get_threads.py
@@ -31,45 +31,61 @@ def find_msg(m_id):
 
 n = 0
 #for file_name in sorted(list_of_gzip_files):
+
+out_file = './threads.txt'
+
 for file_name in reversed(sorted(list_of_gzip_files)):
-    with gzip.open(file_name, mode='rb') as input_file:
-        file_content = input_file.readlines()
-        print('File read: ' + file_name)
+    with open(out_file, 'a') as out:
+        with gzip.open(file_name, mode='rb') as input_file:
+            file_content = input_file.readlines()
+            print('File read: ' + file_name)
 
-        for line in file_content:
-            line = line.decode('utf-8')
-            data_dict = json.loads(line)
-            #print(repr(data_dict)[:10000])
-            for k in reversed(sorted(data_dict.keys())):	# не в том месте reversed был
-                v = data_dict[k]
-                k = int(k)
-                m_prev = v['in_reply_to_status_id']	# номер сообщения, на которое ответили
-                if m_prev == None:
-                  if not k in flat_dict.keys(): # Если сообщение не ответ и на него не ссылались раньше
-                    continue
-                  else:
-                    big_dict[flat_dict[k]].append([v['id'], m_prev, v['user']['id'], v['created_at']])
-                #if len(flat_dict.keys()) % 1000 == 0:
-                #  print("Success! %d \t %s" % (len(flat_dict.keys()), ""))
-                orig_msg_id = find_msg(m_prev)		# номер исходного треда в big_dict
-                #if not orig_msg_id == m_prev:
-                #  print("Ura! %d \t %d" % (orig_msg_id, m_prev))
-                t_id = 1
-                if k in flat_dict.keys():
-                    t_id = flat_dict[k]
-                    flat_dict[m_prev] = t_id
-                else:
-                    n += 1
-                    t_id = n
-                    flat_dict[m_prev] = n
-                flat_dict[k] = m_prev
-                big_dict[n] = [[v['id'], m_prev, v['user']['id'], v['created_at']]]
+            for line in file_content:
+                line = line.decode('utf-8')
+                data_dict = json.loads(line)
+                #print(repr(data_dict)[:10000])
+                for k in reversed(sorted(data_dict.keys())):	# не в том месте reversed был
+                    v = data_dict[k]
+                    k = int(k)
+                    m_prev = v['in_reply_to_status_id'] 	# номер сообщения, на которое ответили
+                    m_prev = int(m_prev) if not m_prev == None else None
+                    if m_prev == None:
+                        if not k in flat_dict.keys(): # Если сообщение не ответ и на него не ссылались раньше
+                            pass
+                        else:
+                            big_dict[flat_dict[k]].append([v['id'], m_prev, v['user']['id'], v['created_at']])
+                        # Saving thread to file
+                        # CLeaning big_dict and flat_fict
+                        out.write(json.dumps(big_dict[flat_dict[k]]))
+                        del big_dict[flat_dict[k]]
+                        del flat_dict[k]
+                    #if len(flat_dict.keys()) % 1000 == 0:
+                    #  print("Success! %d \t %s" % (len(flat_dict.keys()), ""))
+                    orig_msg_id = find_msg(m_prev)		# номер исходного треда в big_dict
+                    #if not orig_msg_id == m_prev:
+                    #  print("Ura! %d \t %d" % (orig_msg_id, m_prev))
+                    t_id = 1
+                    if k in flat_dict.keys():
+                        t_id = flat_dict[k]
+                        flat_dict[m_prev] = t_id
+                    else:
+                        n += 1
+                        t_id = n
+                        flat_dict[m_prev] = n
+                        big_dict[t_id] = []
+                    big_dict[t_id].append([v['id'], m_prev, v['user']['id'], v['created_at']])
+                    #if k in flat_dict.keys():
+                    #  print('ura! %d from thread %d' % (k, t_id))
+                    flat_dict[k] = m_prev
 
-    print(list(flat_dict.keys())[:15])
-    print("Total messages referenced: %d" % len(flat_dict))
-    #print(repr(big_dict)[:10000])
-    #print(big_dict)
-    for k, v in big_dict.items():
-        if len(v) > 2:
-            print(str(k) + ': ' + repr(v))
-    #break
+        #print(list(flat_dict.keys())[:15])
+        print("Total messages referenced: %d" % len(flat_dict))
+        #print(repr(big_dict)[:10000])
+        #print(big_dict)
+    #    for k, v in big_dict.items():
+    #        if len(v) > 2:
+    #            print(str(k) + ': ' + repr(v))
+        # for value in list(big_dict.values()):
+        #     if len(value) > 2:
+        #         print(repr(value))
+        #break
